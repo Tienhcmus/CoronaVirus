6815f00671a9268e98929f9b57c5e31c1432612e
Tienhcmus <60434436+Tienhcmus@users.noreply.github.com>
2020-08-26 20:13:36 +0700

    Create Project

---
+++ minhtien/src/main/java/com/covidtracking/minhtien/MinhtienApplication.java
@@ -0,0 +1,13 @@
+package com.covidtracking.minhtien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhtienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhtienApplication.class, args);
+	}
+
+}
---
+++ minhtien/src/test/java/com/covidtracking/minhtien/MinhtienApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covidtracking.minhtien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhtienApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
